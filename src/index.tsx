f2c7e9fac7b39b77f0b9781ef352018bef0795f6
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-09-20 18:42:46 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,12 @@ root.render(
     <Provider store={store}>
         <App/>
     </Provider>
+    <p class="codepen" data-height="300" data-default-tab="html,result" data-slug-hash="mdQQLge" data-user="inoculate23" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/inoculate23/pen/mdQQLge">
+  Three.js Particle Test</a> by inoculate23 (<a href="https://codepen.io/inoculate23">@inoculate23</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
 );
 
 // If you want to start measuring performance in your app, pass a function
